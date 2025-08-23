09c0dc9ca7dd14d7ed31d5921088eed2a74c9023
dashuai009 <1525836294@qq.com>
2025-08-24 02:03:51 +0800

    add  pbrt-v4-cn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import mdx from '@astrojs/mdx';
 import { typst } from 'astro-typst';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://dashuai009.github.io',
-	integrations: [mdx(), sitemap(), typst({
+	integrations: [mdx(), sitemap(),, react(), typst({
       options: {
         remPx: 14,
       },
