a9f1ae962afdb9219ccb2b2de2e02b3c302b4449
dashuai009 <1525836294@qq.com>
2026-09-02 17:04:13 +0800

    Refine C++23 banner layout

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -6,7 +6,7 @@ export const LIGHT_MODE = "light",
 export const DEFAULT_THEME = AUTO_MODE;
 
 // Banner height unit: vh
-export const BANNER_HEIGHT = 35;
+export const BANNER_HEIGHT = 32;
 export const BANNER_HEIGHT_EXTEND = 20;
 export const BANNER_HEIGHT_HOME = BANNER_HEIGHT + BANNER_HEIGHT_EXTEND;
 
